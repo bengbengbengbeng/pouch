0462c6e48c3d2453444e27dd9d5823ca5c417781
霖渊 <caowei.wc@alibaba-inc.com>
2019-06-12 16:59:06 +0800

    test: add integration test result statistics
    Signed-off-by: 霖渊 <caowei.wc@alibaba-inc.com>

--- hack/testing/run_daemon_integration.sh
+++ hack/testing/run_daemon_integration.sh
@@ -25,6 +25,7 @@ integration::run_daemon_test_cases() {
   echo "start pouch daemon integration test..."
   local code=0
   local job_id=$1
+  local logfile=/tmp/test$$.log
 
   cp -rf "${REPO_BASE}/test/tls" /tmp/
 
@@ -39,13 +40,63 @@ integration::run_daemon_test_cases() {
     echo "start to run common test cases"
   fi
   for one in ${testcases}; do
-    "${REPO_BASE}/bin/pouchd-integration-test" -test.v -check.v -check.f "${one}"
+    "${REPO_BASE}/bin/pouchd-integration-test" -test.v -check.v -check.f "${one}" 2>&1 | tee -a $logfile
     ret=$?
     if [[ ${ret} -ne 0 ]]; then
       code=${ret}
     fi
   done
-
+  local passed_count=0
+  local failed_count=0
+  local skipped_count=0
+  local missed_count=0
+  local expected_failures_count=0
+  local panicked_count=0
+  local fixture_panicked_count=0
+  # shellcheck disable=SC2013
+  for i in $(grep -E "^(OOPS:|OK:)\s" ${logfile} | grep -o -e "[0-9]\+ passed" | awk -F ' ' '{print $1}');
+  do
+    passed_count=$((passed_count + i))
+  done
+  # shellcheck disable=SC2013
+  for i in $(grep -E "^(OOPS:|OK:)\s" ${logfile} | grep -o -e "[0-9]\+ skipped" | awk -F ' ' '{print $1}');
+  do
+    skipped_count=$((skipped_count + i))
+  done
+  # shellcheck disable=SC2013
+  for i in $(grep -E "^(OOPS:|OK:)\s" ${logfile} | grep -o -e "[0-9]\+ FAILED" | awk -F ' ' '{print $1}');
+  do
+    failed_count=$((failed_count + i))
+  done
+  # shellcheck disable=SC2013
+  for i in $(grep -E "^(OOPS:|OK:)\s" ${logfile} | grep -o -e "[0-9]\+ MISSED" | awk -F ' ' '{print $1}');
+  do
+    missed_count=$((missed_count + i))
+  done
+  # shellcheck disable=SC2013
+  for i in $(grep -E "^(OOPS:|OK:)\s" ${logfile} | grep -o -e "[0-9]\+ expected failures" | awk -F ' ' '{print $1}');
+  do
+    expected_failures_count=$((expected_failures_count + i))
+  done
+  # shellcheck disable=SC2013
+  for i in $(grep -E "^(OOPS:|OK:)\s" ${logfile} | grep -o -e "[0-9]\+ PANICKED" | awk -F ' ' '{print $1}');
+  do
+    panicked_count=$((panicked_count + i))
+  done
+  # shellcheck disable=SC2013
+  for i in $(grep -E "^(OOPS:|OK:)\s" ${logfile} | grep -o -e "[0-9]\+ FIXTURE-PANICKED" | awk -F ' ' '{print $1}');
+  do
+    fixture_panicked_count=$((fixture_panicked_count + i))
+  done
+  echo "---------Test Result---------"
+  echo "passed case $passed_count"
+  echo "skipped case $skipped_count"
+  echo "failed case $failed_count"
+  echo "expected failures case $expected_failures_count"
+  echo "missed case $missed_count"
+  echo "panicked case $panicked_count"
+  echo "fixture panicked case $fixture_panicked_count"
+  echo "---------Test Result---------"
   integration::stop_local_persist
   integration::stop_pouchd
   set -e
